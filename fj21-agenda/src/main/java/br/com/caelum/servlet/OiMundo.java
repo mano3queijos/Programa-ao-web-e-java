a9ae613724473c280a32bf40dd43aa92391fd517
Emanuel <101946589+mano3queijos@users.noreply.github.com>
2023-03-24 22:53:59 -0300

    Add files via upload

---
+++ fj21-agenda/src/main/java/br/com/caelum/servlet/OiMundo.java
@@ -0,0 +1,41 @@
+package br.com.caelum.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class OiMundo
+ */
+public class OiMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public OiMundo() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Primeira	Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Oi	mundo	Servlet!</h1>");
+		out.println("</body>");
+		out.println("</html>");
+
+	}
+	
+
+}
